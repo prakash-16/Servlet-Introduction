270047c2a9b7af639377bbe0b89c0502690311b1
Prakash Goud <prakash.goud@morningstar.com>
2021-09-22 21:28:07 +0530

    [Prakash] - Created a simple login with predefined user

--- src/main/java/com/bridgelabz/servletintroduction/FirstServlet.java
+++ src/main/java/com/bridgelabz/servletintroduction/FirstServlet.java
@@ -2,14 +2,38 @@ package com.bridgelabz.servletintroduction;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/click")
+@WebServlet(
+		description = "Login Servlet Interface",
+		urlPatterns = {"/LoginServlet"},
+		initParams = {
+				@WebInitParam(name = "user",value = "Karan"),
+				@WebInitParam(name = "pwd", value ="karan123")
+		})
 public class FirstServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		res.getWriter().println("Hello World, this is my first servlet");
+	}
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String pwd = req.getParameter("pwd");
+		String userId = getServletConfig().getInitParameter("user");
+		String correctPwd = getServletConfig().getInitParameter("pwd");
+		if(userId.equals(user) && correctPwd.equals(pwd)) {
+			req.setAttribute("user", user);
+			req.getRequestDispatcher("LoginSuccess.jsp").forward(req, res);
+		}
+		else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("login.html");
+			res.getWriter().println("<font> color=red>Either user name or password is wrong.</font>");
+			rd.include(req, res);
+		}
 	}
 }
